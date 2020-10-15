1dc39dec453c0ebb69b8df50bc5bb1bd598d8041
Douglas Willian <doug.willlian@gmail.com>
2020-10-15 15:31:08 -0300

    insta4 react

--- semana5/insta4 2/src/components/Post/Post.js
+++ semana5/insta4 2/src/components/Post/Post.js
@@ -2,33 +2,54 @@ import React from 'react'
 import './Post.css'
 
 import {IconeComContador} from '../IconeComContador/IconeComContador'
+import {IconeSave} from '../IconeSave/IconeSave'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import iconeSave from '../../img/bookmark.svg'
+import iconeSaveOrange from '../../img/bookmarkOrange.svg'
 
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    numeroComentarios: 0
+    numeroComentarios: 0,
+    save: false,
+    comentarios: [{
+      nomeDoUsuario: "",
+      valorDoComentario: ""
+    }],
+    valorInputComentario: ""
   }
 
+
   onClickCurtida = () => {
-    let novoNumeroCurtidas
-
-    if(this.state.curtido) {
-      novoNumeroCurtidas = this.state.numeroCurtidas - 1
-    } else {
-      novoNumeroCurtidas = this.state.numeroCurtidas + 1
-    }
-
     this.setState({
       curtido: !this.state.curtido,
-      numeroCurtidas: novoNumeroCurtidas
+      numeroCurtidas: this.state.numeroCurtidas + 1
     })
+
+    if(this.state.curtido) {
+      this.setState({
+        numeroCurtidas: this.state.numeroCurtidas - 1
+      })
+    }
+ 
+}
+
+  onClickSave = () => {
+    this.setState({
+      save: !this.state.save
+    })
+
+    if(this.state.save) {
+      this.setState({
+        save: false
+      })
+    }
   }
 
   onClickComentario = () => {
@@ -38,19 +59,42 @@ class Post extends React.Component {
   }
 
   aoEnviarComentario = () => {
+
+    const novoComentario = {
+      valorDoComentario: this.state.valorInputComentario
+    }
+
+    const novoArrayComentario = [novoComentario, ...this.state.comentarios]
+
     this.setState({
       comentando: false,
-      numeroComentarios: this.state.numeroComentarios + 1
+      numeroComentarios: this.state.numeroComentarios + 1,
+      comentarios: novoArrayComentario
     })
   }
 
   render() {
+
+    const listaComentario = this.state.comentarios.map((comentario) => {
+
+    })
+
     let iconeCurtida
+
 
     if(this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
+      
+    }
+
+    let iconeSalvar
+
+    if(this.state.save) {
+      iconeSalvar = iconeSaveOrange
+    } else {
+      iconeSalvar = iconeSave
     }
 
     let componenteComentario
@@ -58,6 +102,8 @@ class Post extends React.Component {
     if(this.state.comentando) {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
+
+    console.log(this.props.fotoPost)
 
     return <div className={'post-container'}>
       <div className={'post-header'}>
@@ -79,6 +125,13 @@ class Post extends React.Component {
           onClickIcone={this.onClickComentario}
           valorContador={this.state.numeroComentarios}
         />
+
+        <IconeSave 
+        icone={iconeSalvar}
+        onClickIcone={this.onClickSave}
+        />
+
+
       </div>
       {componenteComentario}
     </div>
